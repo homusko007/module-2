1593dfdb7c558a6a9b3a71c95bddcdfbf40fddda
Natalia Homusko <100041021+homusko007@users.noreply.github.com>
2022-07-29 11:45:24 +0300

    Update script.js

--- phonebook/phonebook/script/script.js
+++ phonebook/phonebook/script/script.js
@@ -193,6 +193,10 @@ const data = [
 
         return {
             list: table.tbody,
+            logo,
+            btnAdd: buttonGroup.btns[0],
+            formOverlay: form.overlay,
+            form: form.form,
         };
     };
 
@@ -215,9 +219,16 @@ const data = [
         const phoneLink = document.createElement('a');
         phoneLink.href = `tel: ${phone}`;
         phoneLink.textContent = phone;
+        tr.phoneLink = phoneLink;
         tdPhone.append(phoneLink);
 
-        tr.append(tdDel, tdName, tdSurname, tdPhone);
+        const tdEdit = document.createElement('td');
+        const buttonEdit = document.createElement('button');
+        buttonEdit.classList.add('btn', 'btn-success');
+        buttonEdit.textContent = 'Редактировать';
+        tdEdit.append(buttonEdit);
+
+        tr.append(tdDel, tdName, tdSurname, tdPhone, tdEdit);
 
         return tr;
     };
@@ -225,16 +236,44 @@ const data = [
     const renderContacts = (elem, data) => {
         const allRow = data.map(createRow);
         elem.append(...allRow);
+        return allRow;
+    };
+
+
+    const hoverRow = (allRow, logo) => {
+        const text = logo.textContent;
+        allRow.forEach(contact => {
+            contact.addEventListener('mouseenter', () => {
+                logo.textContent = contact.phoneLink.textContent;
+            });
+            contact.addEventListener('mouseleave', () => {
+                logo.textContent = text;
+            });
+        });
     };
 
     const init = (selectorApp, title) => {
         const app = document.querySelector(selectorApp);
         const phoneBook = renderPhoneBook(app, title);
 
-        const { list } = phoneBook;
-
-        renderContacts(list, data);
+        const { list, logo, btnAdd, formOverlay, form } = phoneBook;
         //Функционал
+        const allRow = renderContacts(list, data);
+
+        hoverRow(allRow, logo);
+
+        btnAdd.addEventListener('click', () => {
+            formOverlay.classList.add('is-visible');
+        });
+
+        form.addEventListener('click', event => {
+            event.stopPropagation();
+        })
+
+        formOverlay.addEventListener('click', () => {
+            formOverlay.classList.remove('is-visible')
+        });
+
     };
 
     window.phoneBookInit = init;
